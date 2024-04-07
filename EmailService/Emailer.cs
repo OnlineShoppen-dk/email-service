c18376da27f19f624c3b61bc5edb83b2203ff013
Bressendorff <frederik@bressendorff.eu>
2024-04-07 21:21:21 +0200

    feat: implemented email service that reads a message a RabbitMQ queue, and sends an email.

---
+++ EmailService/Emailer.cs
@@ -0,0 +1,42 @@
+﻿using System.Configuration;
+using System.Net;
+using System.Net.Mail;
+
+public record EmailMessage(string Email, string Subject, string Body);
+
+public static class Emailer
+{
+    public static async Task<bool> SendEmail(EmailMessage message)
+    {
+        return await SendEmail(message.Email, message.Subject, message.Body);
+    }
+
+    public static async Task<bool> SendEmail(string email, string subject, string body)
+    {
+        var mail = ConfigurationManager.AppSettings["EtherealUsername"];
+        var pw = ConfigurationManager.AppSettings["EtherealPassword"];
+
+        var smtpHost = "smtp.ethereal.email";
+        var smtpPort = 587;
+
+        var client = new SmtpClient(smtpHost, smtpPort)
+        {
+            EnableSsl = true,
+            Credentials = new NetworkCredential(mail, pw)
+        };
+        try
+        {
+            var mailMessage = new MailMessage(from: mail,
+                to: email,
+                subject,
+                body);
+            mailMessage.IsBodyHtml = true;
+            await client.SendMailAsync(mailMessage);
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+}
--- EmailService/Program.cs
+++ EmailService/Program.cs
@@ -1,3 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Text.Json;
 
-Console.WriteLine("Hello, World!");
+string QueueContainerHostname = "rabbitqueue";
+var factory = new ConnectionFactory { HostName = QueueContainerHostname };
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "Emailer",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+Console.WriteLine(" [*] Waiting for messages.");
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += async (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    var emailMessage = JsonSerializer.Deserialize<EmailMessage>(message);
+
+    await Emailer.SendEmail(emailMessage);
+    Console.WriteLine($" [x] Received message.\n     To: {emailMessage.Email}\n     Subject: {emailMessage.Subject}\n     Body: {emailMessage.Body}");
+};
+
+channel.BasicConsume(queue: "Emailer",
+                     autoAck: true,
+                     consumer: consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
+
+await Task.Run(() => Thread.Sleep(Timeout.Infinite));
+
